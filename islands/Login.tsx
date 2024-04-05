369a79a8be9b83e25aec3e099876fb5259f90bd9
Alex Lyalin <occvlt@yandex.ru>
2024-04-05 16:06:35 +0400

    :zap: telegram auth

--- islands/Login.tsx
+++ islands/Login.tsx
@@ -1,7 +1,8 @@
 import {Client} from "https://deno.land/x/mtkruto@0.1.200/client/4_client.ts";
 import {StorageLocalStorage} from "https://deno.land/x/mtkruto@0.1.200/storage/2_storage_local_storage.ts";
 import { StorageDenoKV } from "https://deno.land/x/mtkruto@0.1.200/storage/1_storage_deno_kv.ts";
-import {useEffect} from "https://esm.sh/stable/preact@10.19.6/denonext/hooks.js";
+import {useEffect, useRef} from "https://esm.sh/stable/preact@10.19.6/denonext/hooks.js";
+import {createRef} from "https://esm.sh/stable/preact@10.19.6/denonext/preact.mjs";
 
 interface Props {
   API_ID: number | null,
@@ -16,19 +17,28 @@ declare global {
 }
 export default function Login() {
 
+  const telegramWrapperRef = useRef(null);
   function handleAuth(e: unknown) {
     console.log(e);
   }
+
+  useEffect(() => {
+    const scriptElement = document.createElement('script');
+    scriptElement.src = 'https://telegram.org/js/telegram-widget.js?22';
+    scriptElement.setAttribute('data-telegram-login', 'terpimcyclebot');
+    scriptElement.setAttribute('data-size', 'large');
+    scriptElement.setAttribute('data-onauth', 'onTelegramAuth(user)');
+    scriptElement.async = true;
+
+    telegramWrapperRef.current.appendChild(scriptElement);
+  }, []);
 
   useEffect(() => {
     window.onTelegramAuth = handleAuth
   }, []);
 
   return (
-    <div>
-      <script async src="https://telegram.org/js/telegram-widget.js?22" data-telegram-login="terpimcyclebot"
-              data-size="large" data-onauth="onTelegramAuth(user)" data-request-access="write"></script>
-
+    <div ref={telegramWrapperRef}>
     </div>
   );
 }
